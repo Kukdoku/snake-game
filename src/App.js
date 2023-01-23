8792b3d898ba260854c2ea3106fb708e3291cba6
fun zone club <visheshmzn99@gmail.com>
2023-01-23 10:52:06 +0530

    feat: adding dark mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import Board from './components/Board';
-import './App.css';
+import React, { useState } from "react";
 
-const App = () => (
-  <div className="App">
-    <Board></Board>
-  </div>
-);
+import Board from "./components/Board";
+import "./App.css";
+
+const App = () => {
+  const [light, setLight] = useState(true);
+  return (
+    <div className={`App ${light && "light"}`}>
+      <button onClick={() => setLight(!light)}>
+        {light ? "darkMode" : "lightMode"}
+      </button>
+      <Board></Board>
+    </div>
+  );
+};
 
 export default App;
